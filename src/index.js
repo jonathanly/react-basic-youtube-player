4114d946c09426e24887c3ecd8c2167490ffd614
Jonathan Ly <jonathanlyhv@gmail.com>
2017-05-11 14:14:26 +1000

    Refactored App to be a class component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyBa3DDIYkm_ovuhHDrw6a3aUPbEzoExaeI'
 
-const App = () => {
-  return <div>
-    <SearchBar />
-  </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'one piece'}, (videos) => {
+      this.setState({ videos });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
